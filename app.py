bc5db7f7cad9b87cb152357051f0cc02cb4b6855
Hasan <hasanaburayyan21@gmail.com>
2025-06-11 19:09:42 -0400

    new code updates

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def greeting():
-  return "Hello CLOUD 360!"
+  return "Hey this is the updated code for the python app!"
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8181)
